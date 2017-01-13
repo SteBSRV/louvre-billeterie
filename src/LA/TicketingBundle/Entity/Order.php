93f4b4045d222922b4f7d4feb8502190e02bdf8a
Ebizet Steve <ebizetsteve@gmail.com>
2017-01-13 18:14:13 +0100

    Optimisation SensioLab Insight

--- src/LA/TicketingBundle/Entity/Order.php
+++ src/LA/TicketingBundle/Entity/Order.php
@@ -252,10 +252,10 @@ class Order
    */
   public function markAsPaid()
   {
-    if($this->paid == true) {
+    if($this->paid === true) {
       return;
     }
-    
+
     $this->paid = true;
 
     foreach ($this->tickets as $ticket) {
